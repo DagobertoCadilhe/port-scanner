d892ca2630a2b1c434ca21a47bd9c051d8e55580
DagobertoCadilhe <dagoberto.ccadilhe@gmail.com>
2026-09-02 19:56:21 -0300

    fix: removed initial logic now handled by scan_ports()

--- port-scanner.py
+++ port-scanner.py
@@ -12,7 +12,29 @@ common_ports = {
     68: "DHCP-client",
     69: "TFTP",
     80: "HTTP",
-    7:  "Echo",
+    88: "Kerberos",
+    110: "POP3",
+    123: "NTP",
+    135: "MSRPC",
+    137: "NetBIOS-ns",
+    138: "NetBIOS-dgm",
+    139: "NetBIOS-ssn",
+    143: "IMAP",
+    161: "SNMP",
+    162: "SNMP-trap",
+    179: "BGP",
+    194: "IRC",
+    389: "LDAP",
+    443: "HTTPS",
+    445: "SMB",
+    464: "Kerberos-change",
+    465: "SMTPS",
+    514: "Syslog",
+    515: "LPD",
+    587: "SMTP-submission",
+    636: "LDAPS",
+    993: "IMAPS",
+    995: "POP3S",
 } 
 
 def scan_ports(host: str, port:int) -> tuple[bool, int, float]:
@@ -29,23 +51,17 @@ def scan_ports(host: str, port:int) -> tuple[bool, int, float]:
     result = s.connect_ex((host, port))
     finish_time = time.time()
 
-    elapsed_time = (start_time - finish_time)
+    elapsed_time = (finish_time - start_time)
     is_open = (result == 0)
 
     return((is_open, result, elapsed_time))
-
-  if not success == 0:
-    print(f"Port {port} : {port_name}, is opened", end="")
-    print(f" Return {success} & Time {(timef - times ):.1f}ms")
-  else:
-    print(f"Port {port} : {port_name}, is closed", end ="")
-    print(f" Return {success} & Time {(timef - times ):.1f}ms")
-
 
 def main():
 
   status = "None"
   host = "8.8.8.8"
+
+  print(f"Starting Port Scan at : {host}")
 
   for port, port_name in common_ports.items():
     is_opened, error_code, duration = scan_ports(port=port, host=host)
